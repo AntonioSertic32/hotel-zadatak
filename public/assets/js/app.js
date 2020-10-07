34b65b562ae2819726ab092c54f93258f943580c
Antonio <antonio.sertic@vsmti.hr>
2020-10-07 15:42:44 +0200

    treci slajder gotov

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -66,17 +66,18 @@
 /******/ ({
 
 /***/ "./resources/assets/src/js/app.js":
-/***/ (function(module, __webpack_exports__) {
+/***/ (function(module, exports) {
 
-"use strict";
 $(document).ready(function () {
+  /*
   $(".slick-slider-1").slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: false,
-    nextArrow: $(".buttonLeft")
+    nextArrow: $(".buttonLeft"),
   });
+  */
 
   $(".slick-slider-2").slick({
     infinite: true,
@@ -85,6 +86,16 @@ $(document).ready(function () {
     speed: 700,
     prevArrow: $(".news-button-left"),
     nextArrow: $(".news-button-right")
+  });
+
+  $(".slick-slider-3").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 700,
+    prevArrow: $(".reviews-button-left"),
+    nextArrow: $(".reviews-button-right"),
+    dots: true
   });
 });
 
--- resources/assets/src/js/app.js
+++ resources/assets/src/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /*
   $(".slick-slider-1").slick({
     infinite: true,
     slidesToShow: 3,
@@ -6,6 +7,7 @@ $(document).ready(function() {
     arrows: false,
     nextArrow: $(".buttonLeft"),
   });
+  */
 
   $(".slick-slider-2").slick({
     infinite: true,
@@ -14,5 +16,15 @@ $(document).ready(function() {
     speed: 700,
     prevArrow: $(".news-button-left"),
     nextArrow: $(".news-button-right"),
+  });
+
+  $(".slick-slider-3").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    speed: 700,
+    prevArrow: $(".reviews-button-left"),
+    nextArrow: $(".reviews-button-right"),
+    dots: true,
   });
 });
